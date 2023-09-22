9f72aad2602b57c373e8ad047513e03a2b0df6d2
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-22 23:43:58 +0530

    fix in api

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,6 +8,10 @@ import { PromptTemplate } from "langchain/prompts";
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
+    console.log(body, "BODY BODY");
+    const question = body.messages[body.messages.length - 1].content;
+
+    console.log(question, "QUESTION QUESTION");
     const embedandquerypinecone = await fetch(
       `https://daily-beige.cmd.outerbase.io/embedandquery`,
       {
@@ -15,21 +19,26 @@ export async function POST(req: NextRequest) {
         headers: {
           "content-type": "application/json",
         },
-        body: JSON.stringify(body),
+        body: JSON.stringify(question),
       }
     );
     const data = await embedandquerypinecone.json();
     console.log(data);
+    const content = data.matches[0].metadata.pageContent;
+    // 5. Log the number of matches
+    console.log(`Found ${data.matches.length} matches...`);
+    // 6. Log the question being asked
+    console.log(`Asking question: ${question}...`);
     if (data.matches.length) {
       const llm = new OpenAI({
         modelName: "gpt-3.5-turbo",
         streaming: true,
         temperature: 1.0,
       });
-      const promptTemplate = `Use the following pieces of context to answer the question at the end. Try to be brief and to the point if possible.
-{context}
-Question: {question}
-Answer in English:`;
+      const promptTemplate = `Use the following pieces of data to answer the question at the end. Try to be brief and to the point if possible.
+${content}
+Question: ${question}
+`;
       const prompt = PromptTemplate.fromTemplate(promptTemplate);
       const chain = loadQAStuffChain(llm, { prompt });
       console.log(chain, "CHAIN");
@@ -40,7 +49,7 @@ Answer in English:`;
         input_documents: [
           new Document({ pageContent: concatenatedPageContent }),
         ],
-        question: body,
+        question: question,
       });
       // 10. Log the answer
       console.log(`Answer: ${result.text}`);
