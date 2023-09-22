5614ac03a7c55e14a92bd616aa344efae4cc6418
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-23 00:10:56 +0530

    downgraded next.js version

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,7 +5,7 @@ import { OpenAI } from "langchain/llms/openai";
 import { NextRequest, NextResponse } from "next/server";
 import { PromptTemplate } from "langchain/prompts";
 
-export async function POST(req: NextRequest) {
+export async function POST(req: Request) {
   try {
     const body = await req.json();
     console.log(body, "BODY BODY");
